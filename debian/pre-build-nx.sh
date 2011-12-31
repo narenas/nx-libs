86f50fa1187c3910a98f84d437cdf7aaa0b063df
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2011-12-31 16:30:24 +0100

    add /debian folder

---
+++ debian/pre-build-nx.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# create copies of upstream changelogs so that names apply to Debian policy...
+cp -a nxcomp/CHANGELOG nxcomp/changelog
+cp -a nxproxy/CHANGELOG nxproxy/changelog
+
+exit 0
